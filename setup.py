53fb9ca8cd72c3db94ef15ab302f5a3484b86453
hidekuma <d.hidekuma@gmail.com>
2019-06-11 11:21:51 +0900

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='asyncurl',
-      version='0.1',
+      version='0.1.0',
       url='https://github.com/hidden-function/asyncurl',
       license='MIT',
       author='Hidekuma',
@@ -9,6 +9,7 @@ setup(name='asyncurl',
       description='Asynchronous cURL Requests',
       packages=find_packages(exclude=['tests']),
       long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       zip_safe=False,
       keywords=['async', 'asynchronous', 'requests', 'cURL', 'curl'],
       install_requires=[
