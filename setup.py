2fe51208313968c7475adcb0ca4a1d0d3b7c79b6
hidekuma <d.hidekuma@gmail.com>
2019-06-11 17:33:16 +0900

    AsnycURL first Release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='asyncurl',
-      version='0.1.3',
+      version='0.1.4',
       url='https://github.com/hidden-function/asyncurl',
       license='MIT',
       author='Hidekuma',
