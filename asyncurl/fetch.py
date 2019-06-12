bed3e6dfda57af67405d793122d4e20cded64878
hidekuma <d.hidekuma@gmail.com>
2019-06-12 16:01:08 +0900

    Updated asyncurl cores

--- asyncurl/fetch.py
+++ asyncurl/fetch.py
@@ -1,20 +1,31 @@
 import collections
 import requests
 from asyncurl.base import AsyncURLBase, asyncio, uvloop
+import sys
+from memory_profiler import profile
 
 class AsyncURLFetch(AsyncURLBase):
     def __init__(self):
         super().__init__()
 
-    async def _queue_fetch(self, loop, queue, callback):
+    def __getattr__(self, name):
+        return super().__getattr__(name)
+
+    def _validate(self, urls):
+        if not isinstance(urls, collections.Iterable):
+            raise ValueError('urls arg can accept only Iterable')
+
+    async def _queue_fetch(self, loop, callback):
         """
         - call by _queue_execution()
         """
-        while not queue.empty():
-            url = await queue.get()
+        while not self.queue.empty():
+            url = await self.queue.get()
             future = loop.run_in_executor(None, requests.get, url)
-            if callback is not None:
+            if callback:
                 future.add_done_callback(callback)
+            else:
+                future.add_done_callback(self._store_result)
             await future
 
     async def _queue_execution(self, arg_urls, *, callback):
@@ -22,12 +33,12 @@ class AsyncURLFetch(AsyncURLBase):
         - call by parallel_fetch()
         """
         loop = asyncio.get_event_loop()
-        queue = asyncio.Queue()
 
-        for url in arg_urls:
-            queue.put_nowait(url)
+        if arg_urls:
+            for url in arg_urls:
+                self.queue.put_nowait(url)
 
-        tasks = [self._queue_fetch(loop, queue, callback) for i in range(self._worker)]
+        tasks = [self._queue_fetch(loop, callback) for i in range(self._worker)]
         return await asyncio.wait(tasks)
 
     def _store_result(self, future):
@@ -36,19 +47,27 @@ class AsyncURLFetch(AsyncURLBase):
         """
         self._results.append(future.result())
 
-    def parallel(self, urls, *, callback=None):
+    #async def _fetch(self, url, callback):
+    #    r = requests.get(url)
+    #    if callback:
+    #        r = callback(r)
+    #    return r
+
+    #async def _execution(self, arg_urls, callback):
+    #    reqs = [self._fetch(url, callback) for url in arg_urls]
+    #    return await asyncio.wait(reqs)
+
+    def parallel(self, urls=[], *, callback=None, queue=False):
         """
+        - [ENTRYPOINT]
+        - validation
         - the order of result is nonsequential
         """
-        if not isinstance(urls, collections.Iterable):
-            raise ValueError('can accept args like Iterable')
+        self._validate(urls)
+        self._results.clear() # RESET results
 
-        # reset results
-        self._results.clear()
         loop = asyncio.get_event_loop()
 
-        if callback is None:
-            callback = self._store_result
-
         loop.run_until_complete(self._queue_execution(urls, callback=callback))
+
         return self
