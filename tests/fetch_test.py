b598f8a18ee82ece881fd600a126c6a409e98320
hidekuma <d.hidekuma@gmail.com>
2019-06-13 15:18:19 +0900

    Updated test cases of fetch,base

--- tests/fetch_test.py
+++ tests/fetch_test.py
@@ -2,9 +2,11 @@ import unittest
 import requests
 from collections.abc import Iterable
 from asyncurl.fetch import AsyncURLFetch
+from asyncurl.session import AsyncURLSession
 
+TEST_URL ='http://github.com'
 TEST_URLS = [
-    'http://github.com',
+    TEST_URL
 ]
 
 class AsyncURLFetchTest(unittest.TestCase):
@@ -25,12 +27,15 @@ class AsyncURLFetchTest(unittest.TestCase):
         fetch = AsyncURLFetch()
         fetch.parallel(TEST_URLS, callback=lambda x: print('with callback : {0}'.format(x)))
 
-    def test_parallel_fetch_with_queue(self):
-        print('-------- [with queue] ----------')
+    def test_parallel_fetch_with_session(self):
+        print('-------- [with session] ----------')
         fetch = AsyncURLFetch()
-        results = fetch.parallel(TEST_URLS, queue=True).results
-        print(results)
-        self.assertIsInstance(results, Iterable)
+        session = AsyncURLSession()
+        session.fetch_url = TEST_URL
+        session.fetch_method = 'GET'
+        session.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        fetch.queue.put_nowait(session)
+        print(fetch.parallel().results)
 
 if __name__ == '__main__':
     unittest.main()
