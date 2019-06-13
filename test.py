ba7712f887ea4e71cbd85e7381ef92e06b9ee7af
hidekuma <d.hidekuma@gmail.com>
2019-06-13 15:18:29 +0900

    Update example codes

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
 from asyncurl.fetch import AsyncURLFetch
+from asyncurl.session import AsyncURLSession
 
 TEST_URL = 'http://localhost'
 
 ac_fetch = AsyncURLFetch()
+urls = [TEST_URL]
 
-for x in range(10):
+for x in range(2):
     ac_fetch.queue.put_nowait(TEST_URL)
 
-urls_generator = (TEST_URL for x in range(10))
-urls = [
-    TEST_URL,
-    TEST_URL,
-    TEST_URL,
-    TEST_URL,
-    TEST_URL,
-    TEST_URL,
-    TEST_URL,
-]
+for x in range(2):
+    session = AsyncURLSession()
+    session.fetch_url = TEST_URL
+    session.fetch_method = 'GET'
+    session.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    ac_fetch.queue.put_nowait(session)
 
-print(ac_fetch.parallel().results)
-#print(ac_fetch.parallel(urls, callback=lambda x:print(x)).results)
+#print(ac_fetch.parallel().results)
+print(ac_fetch.parallel(urls, callback=lambda x:print(x)).results)
